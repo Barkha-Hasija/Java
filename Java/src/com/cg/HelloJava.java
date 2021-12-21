816f084a2d4bbd77c9c6c0f8db47c90a7adbde0e
Barkha-Hasija <hasijabarkha123@gmail.com>
2021-12-21 16:44:36 +0530

    commit

--- Java/src/com/cg/HelloJava.java
+++ Java/src/com/cg/HelloJava.java
@@ -3,7 +3,9 @@ package com.cg;
 public class HelloJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello Jenkins!!!!!!!!!!!");
+		System.out.println("Hello Jenkins!!!!!!!!!!!");
+		
 
 	}
 
